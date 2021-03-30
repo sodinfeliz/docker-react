77a1bf4c59cf25ecfb2134db659bb6df543fdeaa
ElliotSu <sodinfeliz@gmail.com>
2021-03-30 14:42:38 +0800

    changed app text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Feature Beanch Yooo!
         </a>
       </header>
     </div>
