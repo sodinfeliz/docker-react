90cce9a3f85e4952e7a37651b0c78c8b79ace1aa
ElliotSu <sodinfeliz@gmail.com>
2021-03-30 14:52:45 +0800

    Fixed text error.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload in feature branch.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Feature Beanch Yooo!
+          Learn React
         </a>
       </header>
     </div>
